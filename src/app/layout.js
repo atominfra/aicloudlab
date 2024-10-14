911de47c8ef5a9db3cf8f42bb9ba82e0d51d3a38
Shryansh <112005671+Shryansh107@users.noreply.github.com>
2024-10-15 01:28:33 +0530

    fix: error in the not supported page for small sized devices and remove favicon (#1)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
-// 'use client'
 import localFont from "next/font/local";
 import "./globals.css";
 import {AuthProvider} from './components/providers'
-import NotSupported from '../app/components/notSupported'
+import RootLayoutClient from '../app/RootLayoutClient'
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,11 +22,12 @@ export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
-      <NotSupported/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <RootLayoutClient>{children}</RootLayoutClient>
+        </AuthProvider>
       </body>
     </html>
   );
