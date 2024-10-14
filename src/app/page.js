0c57bb9c40b9ce80c74f843a64473c2943858cd9
Shryansh <shryansh2024@gmail.com>
2024-10-14 21:23:18 +0530

    chore: added page labels

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,8 @@ import toast, { Toaster } from 'react-hot-toast';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center flex-col gap-8 justify-center h-screen">
+      <div>Landing Page</div>
       <Link href="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Login
       </Link>
