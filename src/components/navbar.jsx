d87016d28b889cf5a2bbee82f27e3193e8ca9fd9
Shryansh <shryansh2024@gmail.com>
2024-10-18 14:02:28 +0530

    fix: logo redirects to dashboard in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,20 +4,21 @@ import React from 'react'
 import ThemeSwitch from './ThemeSwitch'
 import { useTheme } from 'next-themes';
 import { HiUserCircle } from "react-icons/hi2";
+import Link from 'next/link';
 
 export default function Navbar() {
   const { resolvedTheme } = useTheme();
 
   return (
     <Box className=" w-full p-4 flex justify-between select-none ">
-        <Box className=" flex items-center">
+        <Link className=" flex items-center" href={`/dashboard`}>
         <Image 
         src={resolvedTheme === 'dark' ? 'https://res.cloudinary.com/dy8hx2xrj/image/upload/v1728900185/cloud-lab-high-resolution-logo-grayscale-transparent_ba6qdw.png' : 'https://res.cloudinary.com/dsfu8suwl/image/upload/v1729192530/cloud-lab-high-resolution-logo-grayscale-transparent_1_-_Edited_a4pbfi.webp'}
         width={1000}
         height={1000}
         className=' w-20 h-12'
         alt="AI Cloud Lab Logo" />
-      </Box>
+      </Link>
 
       <Box className="flex gap-8 items-center">
       <ThemeSwitch/>
