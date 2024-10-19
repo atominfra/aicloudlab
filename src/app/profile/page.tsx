37393d5fb77994433135987438e6f1c2b89f8693
Shryansh <shryansh2024@gmail.com>
2024-10-19 13:11:31 +0530

    fix: fixes build errors

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,21 +2,30 @@
 import React, { useEffect, useState } from 'react';
 import {
   Box,
-  TextField,
   Button,
   Typography,
 } from '@mui/material';
-import { RiPencilFill } from "react-icons/ri";
-import Navbar from '@/components/navbar';
 import { PiUserCircleFill } from "react-icons/pi";
+import Navbar from '@/components/navbar';
 import { useTheme } from 'next-themes';
-import { useGlobalContext } from '@/context/GlobalContext';
+
+interface User {
+  full_name: string;
+  email: string;
+  phone: string;
+}
 
 export default function ProfilePage() {
   const { resolvedTheme } = useTheme();
+  
+  const [user, setUser] = useState<User | null>(null); 
 
-  const storedUser = localStorage.getItem('user');
-  const user = storedUser ? JSON.parse(storedUser) : null; 
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   // State for form fields
   const [formData, setFormData] = useState({
@@ -27,7 +36,7 @@ export default function ProfilePage() {
   });
 
   // Handle input change
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
@@ -56,52 +65,22 @@ export default function ProfilePage() {
       alert('An error occurred while updating user details');
     }
   };
-  useEffect(()=>{
-    console.log("user",user)
-  },[user])
+
   return (
     <Box className="flex flex-col items-center gap-8 min-h-screen bg-white dark:bg-gray-900 text-[#111827] dark:text-white">
       <Navbar />
       <Box className="flex justify-center items-center gap-8 w-[90vw] h-[70vh]">
         <Box className='w-[25vw] flex justify-center items-center'>
-        <PiUserCircleFill size={200} />
+          <PiUserCircleFill size={200} />
         </Box>
         <Box className="flex flex-col space-y-4 border-l-2 border-[#666666] p-10 pl-16 w-[40vw]">
-          {/* <TextField
-            fullWidth
-            label="Full Name"
-            name="fullName"
-            value={formData.fullName}
-            onChange={handleChange}
-            variant="outlined"
-            InputProps={{
-              className: 'bg-white dark:bg-gray-800 text-[#111827] dark:text-white rounded-[10px]'
-            }}
-            InputLabelProps={{
-              sx: {
-                color: resolvedTheme === "dark"?'white':'black',
-                fontFamily: 'poppins',
-                '&.Mui-focused': { color: resolvedTheme === "dark"?'white':'black' }
-              }
-            }}
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': { borderColor: resolvedTheme === "dark"?'white':'black' },
-                '&:hover fieldset': { borderColor: resolvedTheme === "dark"?'white':'black' },
-                '&.Mui-focused fieldset': { borderColor: resolvedTheme === "dark"?'white':'black' }
-              }
-            }}
-          /> */}
-          <Typography className=' p-4 bg-white dark:bg-gray-800 text-[#111827] dark:text-white border border-black rounded-[10px]'>{user.full_name}</Typography>
-          <Typography className=' p-4 bg-white dark:bg-gray-800 text-[#111827] dark:text-white border border-black rounded-[10px]'>{user.email}</Typography>
-          <Typography className=' p-4 bg-white dark:bg-gray-800 text-[#111827] dark:text-white border border-black rounded-[10px]'>{user.phone}</Typography>
-          {/* <Button
-            endIcon={<RiPencilFill className='dark:hover:text-yellow-500 text-xl ' />}
-            className=" dark:hover:text-yellow-500 font-poppins text-[#111827] dark:text-white  capitalize border-2 border-black"
-            onClick={handleSubmit}
-          >
-            Reset Password
-        </Button> */}
+          {user && (
+            <>
+              <Typography className=' p-4 bg-white dark:bg-gray-800 text-[#111827] dark:text-white border border-black rounded-[10px]'>{user.full_name}</Typography>
+              <Typography className=' p-4 bg-white dark:bg-gray-800 text-[#111827] dark:text-white border border-black rounded-[10px]'>{user.email}</Typography>
+              <Typography className=' p-4 bg-white dark:bg-gray-800 text-[#111827] dark:text-white border border-black rounded-[10px]'>{user.phone}</Typography>
+            </>
+          )}
           <Box className="pt-4">
             <Button
               variant="contained"
