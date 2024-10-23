ceea3675ab9cd0d9203f42e08538ab19c6a552a7
Shryansh <112005671+Shryansh107@users.noreply.github.com>
2024-10-23 12:40:00 +0530

    V0 dev (#17)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
-import RootLayoutClient from '../app/RootLayoutClient'
+import RootLayoutClient from '../app/RootLayoutClient';
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -25,16 +28,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   return (
     <html lang="en" suppressHydrationWarning>
-          <link rel="icon" href="/favicon.ico" />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
-      >
-          <RootLayoutClient>
-            {children}
-            </RootLayoutClient>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* Google Tag Manager Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6R85ZEG6WD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6R85ZEG6WD');
+          `}
+        </Script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}>
+        <RootLayoutClient>
+          {children}
+        </RootLayoutClient>
       </body>
     </html>
   );
