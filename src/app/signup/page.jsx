3d38b391635d222d3b7aa0116cba4c1bb6b98666
Karun Agarwal <singhalkarun@gmail.com>
2024-10-30 19:04:26 +0530

    specify domain in cookie

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -75,8 +75,9 @@ console.log("data",reEnterPassword,data)
 
       if (response.ok) {
         localStorage.setItem('user', JSON.stringify(responseData.data.user)); 
-        localStorage.setItem('access_token', responseData.data.access_token);
-        document.cookie = `access_token=Bearer ${responseData.data.access_token}; path=/`;
+        localStorage.setItem('access_token', responseData.data.access_token);        
+        document.cookie = `access_token=Bearer ${responseData.data.access_token}; path=/; domain=.${window.location.hostname}`;
+        
         window.location.href = '/dashboard' 
       } else {
         toast.error(responseData.message || 'Signup failed');
