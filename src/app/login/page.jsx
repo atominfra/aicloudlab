64ef15509e80f19719867ed6d7cb60c36be3139f
shryansh <shryansh2024@gmail.com>
2024-11-14 21:14:44 +0530

    Temporary save of work in progress

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -83,7 +83,7 @@ export default function Login() {
         document.cookie = `access_token=Bearer ${responseData.data.access_token}; expires=${new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString()}; path=/; domain=.${window.location.hostname}`;
         window.location.href = '/dashboard' 
       } else {
-        console.error("Login failed", responseData);
+        console.error("Login failed", responseData);f
         setLoginError(responseData.message || 'An unexpected error occurred. Please try again.');
       }
     } catch (error) {
--- src/app/notebook/[id]/page.tsx
+++ src/app/notebook/[id]/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 import { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import withAuth from '@/components/withAuth';
 import { useParams } from 'next/navigation';
+import AnchorTemporaryDrawer from '@/components/notebook /hamburger';
+import { SiJupyter } from 'react-icons/si';
+import Link from 'next/link';
+import { PiUserCircleFill } from 'react-icons/pi';
 
 const NotebookPage = () => {
   const { id } = useParams();  // Get notebook id from params
-  const [notebookLink, setNotebookLink] = useState(null);
+  const [notebook, setNotebook] = useState(null);
 
   useEffect(() => {
     const fetchNotebook = async () => {
@@ -22,7 +26,7 @@ const NotebookPage = () => {
         if (response.ok) {
           const data = await response.json();
           console.log("data",data.data.notebook.notebook_url)
-          setNotebookLink(data.data.notebook.notebook_url);  
+          setNotebook(data.data.notebook);  
         } else {
           console.error('Failed to fetch notebook data');
         }
@@ -36,20 +40,42 @@ const NotebookPage = () => {
 
   useEffect(()=>{
     console.log("id",id)
-    console.log("notebookLink",notebookLink)
+    console.log("notebook",notebook)
   })
   return (
-    <div className="flex h-screen">
-      <div className="w-full">
-        {notebookLink && (
+    <div className="flex flex-col h-screen">
+      <Box className="w-full h-[50px] bg-white flex items-center justify-between m-2">
+        <Box className="flex gap-2 items-center pl-4"> 
+          <SiJupyter  className='text-[#ff7c20]' size={40}/>
+          <Box>
+          <Typography className=' text-2xl text-black font-poppins '>{notebook?.name}</Typography>
+            <div className='text-black flex gap-3 font-light font-poppins text-sm'>
+            <span>File</span>
+            <span>Edit</span>
+            <span>View</span>
+            <span>Run</span>
+            <span>kernel</span>
+            <span>Settings</span>
+            </div>
+          </Box>
+        </Box>
+        <Box className="flex gap-8 items-center pr-6">
+      <Link href={'/profile'}>
+      <PiUserCircleFill size={45} className='text-black' />
+      </Link>
+      </Box>
+        {/* <AnchorTemporaryDrawer /> */}
+      </Box>
+      <Box className="w-full h-full">
+        {notebook?.notebook_url && (
           <iframe
-            src={notebookLink}
+            src={notebook?.notebook_url}
             className="w-full h-full"
             sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-downloads"
             title="Notebook"
           />
         )}
-      </div>
+      </Box>
     </div>
   );
 }
