ccf1b44dd2f8033b3a6a4b675d1292d0431bf4e0
shryansh <shryansh2024@gmail.com>
2024-11-15 18:26:11 +0530

    fixes build errors

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Typography, Box, TextField, Select, MenuItem, ButtonBase, CircularProgress } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import { RiArrowDropDownLine } from "react-icons/ri";
