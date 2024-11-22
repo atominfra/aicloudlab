f83be894f2c9a3c030141dec755af70ddc025191
shryansh <shryansh2024@gmail.com>
2024-11-22 22:54:15 +0530

    adds toast to deploy and sync bttn

--- src/app/notebook/[id]/page.tsx
+++ src/app/notebook/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { Box, Button, Popper, Typography } from '@mui/material';
 import withAuth from '@/components/withAuth';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import { AiOutlineSync } from "react-icons/ai";
@@ -13,6 +13,11 @@ import rackCorp from "@/assets/rackCorp.png"
 import Loader from "@/components/loader"; // Import your loader
 import deploy from "@/assets/deploy.png";
 import { FiExternalLink } from "react-icons/fi";
+import CustomButton from '@/components/ui/button';
+import toast, { Toaster } from "react-hot-toast";
+
+import { IoIosArrowBack } from "react-icons/io";
+import NoteBookLoader from '@/components/notebook /notebookLoader';
 type Notebook = {
   id: number;
   name: string;
@@ -26,7 +31,7 @@ const NotebookPage = () => {
   const [open, setOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const popperId = open ? 'simple-popper' : undefined;
-
+  const router = useRouter();
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
     setOpen((previousOpen) => !previousOpen);
@@ -62,49 +67,54 @@ const NotebookPage = () => {
 
   return (
     <div className="flex flex-col h-screen">
-      <Box className="w-full bg-white flex items-center justify-between p-2">
-        <Box className="flex items-center px-4"> 
+      <Box className="w-full bg-white flex items-center justify-between px-10   py-4 shadow-md z-[10]">
+        <Box className="flex items-end"> 
           <Link className="flex items-center" href={`/dashboard`}>
             <Image 
               src='https://res.cloudinary.com/dsfu8suwl/image/upload/v1729192530/cloud-lab-high-resolution-logo-grayscale-transparent_1_-_Edited_a4pbfi.webp'
               width={1000}  
               height={1000}
-              className='w-20 h-12'
+              className=' w-[63px] h-[38px] '
               alt="AI Cloud Lab Logo" 
             />
           </Link>          
           <Box>
-            <Typography className='pl-2 text-2xl text-black'>
+            <Typography className='pl-4 text-2xl text-black'>
               {notebook?.name}
             </Typography>
           </Box>
         </Box>
         <Box className="flex gap-8 items-center">
           <button
-            className={`bg-white hover:bg-[#1976D2] h-[38px w-[108px] hover:text-white text-black shadow-none text-[16px] font-semibold rounded-[10px] border px-2 py-1 border-gray-300 flex gap-1 items-center justify-center`}
-            onClick={() => {}}
+            className={`bg-white hover:bg-[#1976D2] h-[38px]  hover:text-white text-black shadow-none text-[16px] font-semibold rounded-[10px] border px-4 py-1 border-gray-300 flex gap-2 items-center justify-center`}
+            onClick={() => toast.success('Coming Soon')}
             style={{ textTransform: 'none' }}
           >
             <AiOutlineSync />
             Sync
           </button>
           <button
-            className={`bg-white hover:bg-[#1976D2] hover:text-white text-black shadow-none text-[15px] font-semibold rounded-[10px] ${open ? "border border-black":"border"} px-2 py-1 h-[38px] w-[108px] border-gray-300 flex gap-1 items-center justify-center`}
-            onClick={handleClick}
+            className={`bg-white   text-black shadow-none text-[16px] font-semibold rounded-[10px] ${open ? "border border-black":"border border-gray-300 hover:bg-[#1976D2] hover:text-white"} px-4 py-1 h-[38px]   flex gap-2 items-center justify-center `}
+            onClick={() => toast.success('Coming Soon')}
             style={{ textTransform: 'none' }}
           >
-            <FaRocket />
+            <FaRocket className='pt-[2px]' />
             Deploy
           </button>
-          <Link href={'/profile'}>
-            <PiUserCircleFill size={45} className='text-black' />
-          </Link>
+          <button
+            className={` bg-[#1976D2] h-[38px] w-[142px] text-white  shadow-none text-[16px] font-semibold rounded-[10px] border px-2 py-1 border-gray-300 flex gap-2 items-center justify-center ml-6`}
+            onClick={()=> router.push('/dashboard')}
+            style={{ textTransform: 'none' }}
+          >
+            <IoIosArrowBack />
+            Dashboard
+          </button>
         </Box>
       </Box>
       <Box className="w-full h-full relative">
         {isLoading && (
           <Box className="absolute inset-0 flex items-center justify-center bg-white">
-            <Loader /> 
+            <NoteBookLoader />         
           </Box>
         )}
         {notebook?.notebook_url && (
@@ -118,7 +128,7 @@ const NotebookPage = () => {
         )}
       </Box>
       <Popper id={popperId} open={open} anchorEl={anchorEl} placement='bottom-end'>
-        <Box className='border p-4 flex flex-col gap-6 bg-white text-black m-1 rounded-[10px]'>
+        <Box className='border p-4 flex flex-col gap-6 bg-white text-black mt-2 rounded-[10px]'>
           <Box className="flex justify-between gap-2 items-center">
           <Image alt='RackCorp' src={rackCorp} width={63} height={15.27}/> 
           <Typography className='  w-[204px] bg-white dark:bg-gray-800 text-[#111827] dark:text-white border border-[#cccccc] rounded-[10px]  h-[37px] flex '>
@@ -133,6 +143,7 @@ const NotebookPage = () => {
           <Box className='hover:cursor-pointer'>Stop Server</Box>
         </Box>
       </Popper>
+      <Toaster position="bottom-right" />
     </div>
   );
 };
