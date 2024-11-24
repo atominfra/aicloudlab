03550a49e80a31ec27671cc82eb175416e41b13b
shryansh <shryansh2024@gmail.com>
2024-11-24 19:58:58 +0530

    removes pop over from select package button

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -24,17 +24,6 @@ const CreateNotebook = () => {
   const { resolvedTheme } = useTheme();
   const [showModal, setShowModal] = useState(false);
   const [isNameTouched,setIsNameTouched] = useState(false)
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -210,24 +199,6 @@ const CreateNotebook = () => {
               <MenuItem value="3.9">Python 3.9</MenuItem>
               <MenuItem value="3.10">Python 3.10</MenuItem>
             </Select>
-            <Popover
-              id="mouse-over-popover"
-              sx={{ pointerEvents: 'none', m: 1 }}
-              open={open}
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              onClose={handlePopoverClose}
-              disableRestoreFocus
-            >
-              <Typography sx={{ p: 1 }}>Feature coming soon</Typography>
-            </Popover>
           <Select
           disabled
             fullWidth
@@ -237,8 +208,6 @@ const CreateNotebook = () => {
             onChange={handleChange}
             displayEmpty
             variant="outlined"
-            onMouseEnter={handlePopoverOpen}
-            onMouseLeave={handlePopoverClose}
             className="bg-white dark:bg-gray-800 text-[#111827] dark:text-white font-poppins rounded-[10px]"
             IconComponent={(props) => (
               <RiArrowDropDownLine {...props} style={{ color: resolvedTheme === "dark"?'white':'black', fontSize: '30px' }} />
