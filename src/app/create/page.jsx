82f3b873ccb0a8e01296bf2b2e1d4a2b1ad82f2b
Karun Agarwal <singhalkarun@gmail.com>
2024-11-25 09:44:44 +0530

    fix-build-issues

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -208,8 +208,6 @@ const CreateNotebook = () => {
             onChange={handleChange}
             displayEmpty
             variant="outlined"
-            onMouseEnter={handlePopoverOpen}
-            onMouseLeave={handlePopoverClose}
             className="bg-white dark:bg-gray-800 text-[#111827] dark:text-white font-poppins rounded-[10px]"
             IconComponent={(props) => (
               <RiArrowDropDownLine {...props} style={{ color: resolvedTheme === "dark"?'white':'black', fontSize: '30px' }} />
