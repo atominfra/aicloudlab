9ce372afc9bc3a98addb9a0b53cf3b16047de712
Prabhmeet Singh <98380208+Prabhmeetchef@users.noreply.github.com>
2024-11-26 13:22:12 +0530

    Upgrade modal is changed

--- src/components/creditsModal.tsx
+++ src/components/creditsModal.tsx
@@ -11,23 +11,49 @@ export default function CreditsModal({ showModal, onClose }:{
       onClose={onClose}
       className="w-full h-full justify-items-center content-center"
     >
-      <Box className="p-8 bg-white shadow-xl rounded-2xl flex flex-col items-start justify-start lg:w-[35vw] m-4 gap-4">
-        <Typography className="text-black font-poppins font-semibold text-[19px] lg:text-2xl">
+      <Box className="py-6 px-8 bg-white shadow-xl rounded-3xl flex flex-col items-start justify-start lg:w-[42.2vw] m-2 gap-2">
+        <Typography className="text-black font-poppins font-semibold text-[19px] lg:text-[22px]">
           You are on free plan
         </Typography>
-        <Typography className="text-gray-600 font-poppins text-[15px] lg:text-lg ">
+        <Typography className="text-gray-500 font-poppins text-[15px] lg:text-[16px] ">
           Upgrade to Pro to create more notebooks
         </Typography>
+        <Box className='my-4 w-full flex justify-between gap-6'>
+          <div className="bg-[#F5F6F6] w-[48%] rounded-lg px-4 py-[15px]">
+            <div className="flex items-center justify-between">
+            <Typography className="font-semibold font-poppins lg:text-[20px]">
+              Free
+            </Typography>
+            <div className="font-poppins font-semibold text-[6px] lg:text-[8px] border-black border-[1px] py-[2px] px-[4px] rounded-full">Current Plan</div>
+            </div>
+            <Typography className="my-2 text-[18px] lg:text-[25px] font-poppins pb-4">
+            ₹0 <span className="text-[10px] text-gray-600">/month</span>
+            </Typography>
+            <ul className="list-disc pl-5 font-poppins lg:text-[16px] text-[13px]">
+              <li>
+                Access 1 notebook
+              </li>
+            </ul>
+          </div>
+          <div className="bg-[#E8F1FB] w-[48%] rounded-lg px-4 py-[15px]">
+            <div>
+            <Typography className="font-semibold font-poppins lg:text-[20px]">
+              Pro
+            </Typography>
+            </div>
+            <Typography className="my-2 text-[18px] lg:text-[25px] font-poppins pb-4">
+            ₹1000 <span className="text-[10px] text-gray-600">/month</span>
+            </Typography>
+            <ul className="list-disc pl-5 font-poppins lg:text-[16px] text-[13px]">
+              <li>Access up to 5 notebooks</li>
+            </ul>
+          </div>
+        </Box>
         <Box className="flex w-full justify-between gap-4 pt-4">
-          <CustomButton
-            text="Maybe later"
-            onclickhandler={onClose}
-            customCss="w-[50%] bg-[#e3e3e3] text-black shadow-none text-[15px] lg:text-[18px]"
-          />
           <CustomButton
             text="Upgrade"
             onclickhandler={() => toast.success('Coming Soon')}
-            customCss="w-[50%] bg-[#1976D2] text-white text-[15px] lg:text-[16px]"
+            customCss="w-[100%] bg-gradient-to-b from-[#1976D2] to-[#0D3D6C] text-white text-[15px] lg:text-[16px] p-2"
           />
         </Box>
       <Toaster position="bottom-right" />
