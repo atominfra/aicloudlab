f79b686b42538b5ef91fac121320a76d5b5b18fa
Shryansh <shryansh2024@gmail.com>
2025-01-04 14:24:13 +0530

    removes title from nodes page

--- src/app/dashboard/nodes/page.tsx
+++ src/app/dashboard/nodes/page.tsx
@@ -59,8 +59,8 @@ export default function NodesPage() {
 
   return (
     <div className="p-4  bg-neutral-100 lg:h-screen h-[92dvh]   justify-center items-center">
-      <div className="flex items-center justify-between lg:mb-6 mb-5 h-[6vh]">
-        <h1 className="text-lg lg:text-2xl font-semibold">Nodes</h1>
+      <div className="flex items-center justify-end lg:mb-6 mb-5 h-[6vh]">
+        {/* <h1 className="text-lg lg:text-2xl font-semibold">Nodes</h1> */}
         <Button className="bg-blue-600" onClick={()=> router.push("/create/node")} disabled={!node_page_status}>
           <span className="">+</span>
           Create
