65cb078eea5a5850e04b8185c81f895819571351
Shryansh <shryansh2024@gmail.com>
2025-01-09 14:00:02 +0530

    renames signin routes to login

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -8,8 +8,8 @@ import { AuthProvider } from '@/context/AuthContext';
 
 export default function Providers({ children }) {
   const pathname = usePathname();
-  const hideSidebar = pathname === "/signin" || pathname === "/signup";
-  const hideMobileNavs = pathname === "/signin" || pathname === "/signup";
+  const hideSidebar = pathname === "/login" || pathname === "/signup";
+  const hideMobileNavs = pathname === "/login" || pathname === "/signup";
 
   return (
     <>
