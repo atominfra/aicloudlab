09800479d296ed850c2db8fc5d36074b4454f6de
Shryansh <shryansh2024@gmail.com>
2025-01-15 22:53:17 +0530

    feat: add Server icon and update services section in NodePage

--- src/app/project/[id]/node/[nodeId]/page.tsx
+++ src/app/project/[id]/node/[nodeId]/page.tsx
@@ -7,7 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import withAuth from '@/components/withAuth'
 import Image from "next/image"
 import { Typography } from "@mui/material"
-import { FolderOpen } from "lucide-react"
+import { FolderOpen, Server } from "lucide-react"
 import Loader from "@/components/loader"
 import { useApp } from "@/context/AppContext"
 import { getAllNodeServices } from "@/app/api/services/api"
@@ -80,12 +80,17 @@ const NodePage = ({ params }: { params: { nodeId: string } }) => {
     return <Loader/>
   }
 
-  return (
+return (
     <div className="p-4 bg-neutral-100 min-h-screen">
       <div className="flex justify-between items-center mb-6">
         {/* @ts-expect-error build error */}
         <h1 className="text-2xl font-semibold">{nodeData?.name}</h1>
         <Button onClick={()=> router.push(`/create/service?projectId=${projectId}`)} className="bg-blue-600">Create Service</Button>
+      </div>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-lg font-medium text-[#111827]">
+          Services Running
+        </h2>
       </div>
       <div className="grid grid-cols-1 gap-1">
         {services && services.length>0 ? services.map((service) => (
@@ -93,7 +98,7 @@ const NodePage = ({ params }: { params: { nodeId: string } }) => {
           <ServiceCard key={service.id} {...service}/>
         )) : <>
             <div className="flex flex-col justify-center items-center lg:h-[80vh] h-[70dvh]  w-full">
-              <FolderOpen className="w-[100px] h-[100px] text-neutral-200" />
+              <Server className="w-[100px] h-[100px] text-neutral-200" />
               <Typography variant="body1" className="text-gray-400 mb-4 px-6">
                 No Services yet.
               </Typography>
