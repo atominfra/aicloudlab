6d42fe1af092fb3963b31b9907972f368b9e95f8
Shryansh <shryansh2024@gmail.com>
2025-01-16 00:48:40 +0530

    fix: update label for selected project in CreateService form

--- src/app/create/service/page.tsx
+++ src/app/create/service/page.tsx
@@ -384,7 +384,7 @@ const CreateService: React.FC<CreateServiceProps> = () => {
       <form className="space-y-6 max-w-2xl md:mx-auto pb-6 mx-4" onSubmit={handleSubmit}>
       <div className="">
           <label htmlFor="service-name" className="text-sm pl-2 font-medium text-[#374151]">
-            Selected Project*
+             Project*
           </label>
           {!loadingProject ? 
           <div className="relative">
