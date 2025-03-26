74b975f95ae4f035a6e6a0c696e8621abf1ee4c6
shryansh <shryansh2024@gmail.com>
2025-03-26 20:54:20 +0530

    feat: update download file names for kube config and private key in cluster card

--- src/components/cluster-card.tsx
+++ src/components/cluster-card.tsx
@@ -79,7 +79,7 @@ export function ClusterCard({ cluster_id, cluster_name, cluster_type, status, no
       const url = window.URL.createObjectURL(blob)
       const a = document.createElement("a")
       a.href = url
-      a.download = "config.txt" // Set file name for kube config
+      a.download = `${cluster_name}_config_key.pem` // Set file name for kube config
       document.body.appendChild(a)
       a.click()
       a.remove()
@@ -103,7 +103,7 @@ export function ClusterCard({ cluster_id, cluster_name, cluster_type, status, no
       const url = window.URL.createObjectURL(blob)
       const a = document.createElement("a")
       a.href = url
-      a.download = "private_key.pem" // Set file name
+      a.download = `${cluster_name}_private_key.pem` // Set file name
       document.body.appendChild(a)
       a.click()
       a.remove()
